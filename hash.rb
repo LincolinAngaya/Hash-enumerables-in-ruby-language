d04b1b4038943d8e3d0391fda25682bd9e4c5e98
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-08-17 14:46:09 +0300

    done

--- hash.rb
+++ hash.rb
@@ -1,4 +1,4 @@
-reuire "pry"
+require "pry"
 
 book =  {
     title:'intro to ruby programming beginner',
@@ -8,3 +8,15 @@ book =  {
     publisher: 'codemy.com',
     description:'great introduction to ruby for new programmers'
 }
+#book.each{ |b| binding.pry}
+# book.each_key{ |b| binding.pry}
+#book.each_value{ |b| binding.pry}
+# puts book.keys
+# puts book.values
+
+#  title = book.select{|b,c| b =="intro to ruby programming beginner"}
+#  puts book.include?(:prices)
+# puts book.index("codemy.com")
+binding.pry
+
+#converting hash to an array we use e.g <b>book.to_a</b>
