d2e13fc9c4b5b9e85ee957dae1bec6fb01f5b680
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-08-17 14:25:26 +0300

    add new ruby file

---
+++ hash.rb
@@ -0,0 +1,10 @@
+reuire "pry"
+
+book =  {
+    title:'intro to ruby programming beginner',
+    prices: 23.29,
+    aurthor:'john elder',
+    rating:3.5,
+    publisher: 'codemy.com',
+    description:'great introduction to ruby for new programmers'
+}
