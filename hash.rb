e9307305c38c65d6795b5ba7f25695b0219c2df4
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-08-17 14:53:36 +0300

    convert lower case to uppercase

--- hash.rb
+++ hash.rb
@@ -1,13 +1,13 @@
 require "pry"
 
-book =  {
-    title:'intro to ruby programming beginner',
-    prices: 23.29,
-    aurthor:'john elder',
-    rating:3.5,
-    publisher: 'codemy.com',
-    description:'great introduction to ruby for new programmers'
-}
+# book =  {
+#     title:'intro to ruby programming beginner',
+#     prices: 23.29,
+#     aurthor:'john elder',
+#     rating:3.5,
+#     publisher: 'codemy.com',
+#     description:'great introduction to ruby for new programmers'
+# }
 #book.each{ |b| binding.pry}
 # book.each_key{ |b| binding.pry}
 #book.each_value{ |b| binding.pry}
@@ -17,6 +17,21 @@ book =  {
 #  title = book.select{|b,c| b =="intro to ruby programming beginner"}
 #  puts book.include?(:prices)
 # puts book.index("codemy.com")
-binding.pry
+# binding.pry
+#book.each{ |b| binding.pry}
+# book.each_key{ |b| binding.pry}
+#book.each_value{ |b| binding.pry}
+# puts book.keys
+# puts book.values
+
+#  title = book.select{|b,c| b =="intro to ruby programming beginner"}
+#  puts book.include?(:prices)
+# puts book.index("codemy.com")
+
 
 #converting hash to an array we use e.g <b>book.to_a</b>
+ #converting hash to an array we use e.g <b>book.to_a</b>
+
+
+  
+  binding.pry
